b2c6d37007b89238e32863e67d03cf58e0562e54
Semjons <semjons.voronovs@inbox.lv>
2021-04-25 17:56:36 +0300

    error catch fix

--- src/consumer.js
+++ src/consumer.js
@@ -8,7 +8,13 @@ const connectRabbitMq = async () => {
     try {
         const conn = await amqp.connect(process.env.RABBITMQ)
         const ch = await conn.createChannel()
-        await client.connect()
+
+        try {
+            await client.connect()
+        } catch (err) {
+            await conn.close()
+            throw Error(err)
+        }
 
         const database = client.db(process.env.DB_NAME)
         const collection = database.collection(process.env.DB_COLLECTION)
@@ -19,27 +25,34 @@ const connectRabbitMq = async () => {
 
         ch.consume(queue, async (msg) => {
             const data = JSON.parse(msg.content.toString())
-            console.log('[MSG]:', data)
 
             try {
-                collection.insertOne(data)
+                await collection.insertOne(data)
+                console.log('[MSG]:', data)
             } catch (err) {
-                console.error('[INSERT ERROR]: %s, [DATA]: %s', err, data)
+                console.error('[INSERT ERROR]: %s, [DATA]: %s', err.message, data)
+                
+                try {
+                    await conn.close()
+                } catch (err) {
+                    console.log('[AMQP]: closing error:', err.message)
+                }
             }
         }, {
             noAck: true
         })
 
-        conn.on('error', function (err) {
-            console.log('[AMQP]: conn error:', err)
+        conn.on('error', (err) => {
+            console.log('[AMQP]: conn error:', err.message)
         })
 
-        conn.on('close', () => {
+        conn.on('close', async () => {
+            await client.close()
             console.log('[AMQP]: conn closed, reconnecting in 1s')
             setTimeout(connectRabbitMq, 1000)
         })
     } catch (err) {
-        console.error('[CONNECTION ERROR]:', err)
+        console.error('[CONNECTION ERROR]:', err.message)
         console.log('Reconnecting in 1s')
         setTimeout(connectRabbitMq, 1000)
     }
